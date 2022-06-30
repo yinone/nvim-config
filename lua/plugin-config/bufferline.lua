c98ee57589b747135dc1fcf050a1ef1f6530e158
eleven <eleven.image@gmail.com>
2022-06-30 18:00:39 +0800

    feat: bufferline

--- lua/plugin-config/bufferline.lua
+++ lua/plugin-config/bufferline.lua
@@ -12,8 +12,9 @@ bufferline.setup(
       -- 关闭 Tab 的命令，这里使用 moll/vim-bbye 的 :Bdelete 命令
       close_command = 'Bdelete! %d',
       right_mouse_command = 'Bdelete! %d',
-      numbers = 'both',
-      -- 侧边栏配置
+      numbers = 'both', -- 侧边栏配置
+      sort_by = 'insert_after_current',
+      show_tab_indicators = true,
       -- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
       offsets = {
         {
